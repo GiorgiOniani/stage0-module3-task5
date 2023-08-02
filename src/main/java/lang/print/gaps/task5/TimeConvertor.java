d0547e27984b2fc70234184686ce95e6284fd3b1
giorgi.oniani.2 <giorgi.oniani.2@btu.edu.ge>
2023-08-02 17:12:33 +0400

    0

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        minutes = 11.5F;
+        double seconds = minutes * 60.0;
+        System.out.println(seconds);
+
     }
 }
